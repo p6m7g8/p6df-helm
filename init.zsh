c7ae1b7cc3c0861e29593aa0d56a8e17766b7a8f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-09-20 11:37:51 -0400

    feat(api): +p6df::modules::helm::external-dashboard::chart::add(), p6df::modules::helm::jenkins::chart::add()

--- init.zsh
+++ init.zsh
@@ -42,10 +42,14 @@ p6df::modules::helm::external::brew() {
 ######################################################################
 p6df::modules::helm::langs() {
 
+  helm repo add bitnami              https://charts.bitnami.com/bitnami
+  helm repo add incubator            https://storage.googleapis.com/kubernetes-charts-incubator
+  helm repo add stable               https://kubernetes-charts.storage.googleapis.com
+  helm repo add kubernetes-dashoard  https://kubernetes.github.io/dashboard/
+  helm repo add jenkinsci            https://charts.jenkins.io
+  helm repo add nginx                https://helm.nginx.com/stable
 }
 
-#
-#
 ######################################################################
 #<
 #
@@ -72,6 +76,13 @@ p6df::prompt::helm::line() {
     p6_helm_prompt_info
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::helm::kubernetes::dashboard::token()
+#
+#>
+######################################################################
 p6df::modules::helm::kubernetes::dashboard::token() {
 
   local secret
@@ -80,8 +91,48 @@ p6df::modules::helm::kubernetes::dashboard::token() {
   kubectl -n kube-systemd describe secret "$secret" | awk '/^token/ { print $2 }'
 }
 
-p6df::modules::helm::jenkins::admin:password() {
+######################################################################
+#<
+#
+# Function: p6df::modules::helm::jenkins::admin::password()
+#
+#>
+######################################################################
+p6df::modules::helm::jenkins::admin::password() {
 
-  printf $(kubectl get secret --namespace jenkins jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);p6_echo
+  printf $(kubectl -n jenkins get secret jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);p6_echo
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::helm::external-dashboard::chart::add(zone_id, role_arn)
+#
+#  Args:
+#	zone_id - 
+#	role_arn - 
+#
+#>
+######################################################################
+p6df::modules::helm::external-dashboard::chart::add() {
+  local zone_id="$1"
+  local role_arn="$2"
+
+  helm install -n kube-system bitnami/external-dns external-dns --set policy=sync --set txtOwnerId=$zone_id' --set aws.assumeRoleArn=$role_arn --set log-level=debug
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::helm::jenkins::chart::add()
+#
+#>
+######################################################################
+p6df::modules::helm::jenkins::chart::add() {
+
+  local str
+  str=$(p6_template_process "share/jenkins-chart-values.yaml.in" "URL=x")
+  p6_file_write "/tmp/jenkins-chart-values.yaml"
+
+  helm install -f "/tmp/jenkins-chart-values.yaml" jenkins jenkinsci/jenkins  
+}
